9e1299923c8db0ef71b3aa78aa471baec630e11f
cz38 <cz38@students.cs.ubc.ca>
2025-01-10 19:22:53 -0800

    Push

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -1,32 +1,29 @@
 def bound_to_180(angle):
-    """Bounds the provided angle between [-180, 180) degrees.
+    # mod operator
+    angle_new = angle % 360
+    if (angle_new >= 180):
+        return float(angle_new - 360)
+    else:
+        return float(angle_new)
 
-    e.g.)
-        bound_to_180(135) = 135.0
-        bound_to_180(200) = -160.0
-
-    Args:
-        angle (float): The input angle in degrees.
-
-    Returns:
-        float: The bounded angle in degrees.
-    """
-    return 0
+    # if (-180 <= angle < 180):
+    #     return float(angle)
+    # elif (180 <= angle <= 360):
+    #     return float(angle - 360)
+    # elif (angle > 360):
+    #     return float(angle - (angle // 360) * 360)
+    # else:
+    #     return float(360 + angle)
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
-    """Determines whether an angle is between two other angles.
 
-    e.g.)
-        is_angle_between(0, 45, 90) = True
-        is_angle_between(45, 90, 270) = False
+    if (second_angle - first_angle > 180):
+        temp_second_angle = second_angle
+        second_angle = first_angle
+        first_angle = temp_second_angle - 360
 
-    Args:
-        first_angle (float): The first bounding angle in degrees.
-        middle_angle (float): The angle in question in degrees.
-        second_angle (float): The second bounding angle in degrees.
-
-    Returns:
-        bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
-    """
-    return True
+    if (first_angle <= middle_angle <= second_angle):
+        return True
+    else:
+        return False
--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -1,15 +1,49 @@
+import pytest
 from standard_calc import bound_to_180, is_angle_between
 
 
 """ Tests for bound_to_180() """
 
 
-def test_bound_basic1():
-    assert bound_to_180(0) == 0
+@pytest.mark.parametrize("input_value, expected", [
+    (0, 0),
+    (180, -180.0),
+    (-180, -180.0),
+    (190, -170.0),
+    (-190, 170.0),
+    (360, 0.0),
+    (-360, 0.0),
+    (540, -180.0),
+    (-540, -180.0),
+    (730, 10.0),
+])
+def test_bound_to_180(input_value, expected):
+    assert bound_to_180(input_value) == expected
+
+# def test_bound_basic1():
+#     assert bound_to_180(0) == 0
 
 
 """ Tests for is_angle_between() """
 
 
-def test_between_basic1():
-    assert is_angle_between(0, 1, 2)
+@pytest.mark.parametrize("first_angle, middle_angle, second_angle, expected", [
+    (0, 1, 2, True),
+    (0, 0, 0, True),
+    (0, 5, 10, True),
+    (0, 10, 10, True),
+    (-10, -5, -1, True),
+    (5, 2, 10, False),
+    (-10, -3, -5, False),
+    (0, 45, 90, True),
+    (45, 90, 270, False),
+    (460, 500, 702, False),
+    (460, 461, 462, True),
+    (0, 360, 10, False),
+])
+def test_is_angle_between(first_angle, middle_angle, second_angle, expected):
+    assert is_angle_between(first_angle, middle_angle, second_angle) == expected
+
+
+# def test_between_basic1():
+#     assert is_angle_between(0, 1, 2)
